256ba1ff59247f8ddc7f98b4f0f6c4db48cffa45
jwabbitt <jrabbitt1110@gmail.com>
2016-03-31 08:53:01 -0600

    Adds testing and fixes a bug in lib/todos.js

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
-'use strict';
+'use strict'
 
-import config from 'config';
-import express from 'express';
-import bodyParser from 'body-parser';
-import api from './routes/';
+import path from 'path'
+import config from 'config'
+import express from 'express'
+import bodyParser from 'body-parser'
+import api from './routes/'
 
-const app = express();
-const PORT = config.port;
+const app = express()
+const PORT = config.port
+const PUBLIC_DIR = path.join(__dirname, 'public')
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
-app.use('/api', api);
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded())
+app.use('/api', api)
+app.use(express.static(PUBLIC_DIR))
+
+app.use((err, req, res, next) => {
+  res.status(500).json({
+    error: err.message
+  })
+})
 
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-});
+  console.log(`Server listening on port ${PORT}`)
+})
+
+export default app
